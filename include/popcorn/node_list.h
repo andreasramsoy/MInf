4425bfe096cfecd3e2a9d44df1eadf196c8f9b93
Andreas Ramsøy <s1736273@sms.ed.ac.uk>
2021-02-27 19:54:46 +0000

    Bug fixes

--- include/popcorn/node_list.h
+++ include/popcorn/node_list.h
@@ -163,7 +163,7 @@ struct pcn_kmsg_transport* string_to_transport(char* protocol) {
     transport = transport_list_head;
     printk(KERN_DEBUG "string_to_transport called pointer %p\n", transport->transport_structure);
     printk(KERN_DEBUG "string_to_transport called string  %s\n", transport->transport_structure->name);
-    printk(KERN_DEBUG "string_to_transport called comflse %d\n", strcmp("socket", "socket");
+    printk(KERN_DEBUG "string_to_transport called comflse %d\n", strcmp("socket", "socket"));
     printk(KERN_DEBUG "string_to_transport called compare %d\n", strcmp(transport->transport_structure->name, protocol));
 
     if (strcmp(transport->transport_structure->name, protocol) == 0) {
@@ -353,7 +353,7 @@ int add_node(struct message_node* node) { //function for adding a single node to
     }
 
     printk(KERN_DEBUG "Initialising communications for node\n");
-    printk(KERN_DEBUG "Transport for the node initialised?    %d\n", node->transport.is_initialised);
+    printk(KERN_DEBUG "Transport for the node initialised?    %d\n", node->transport->is_initialised);
     printk(KERN_DEBUG "Transport for the node initialised ptr %p\n", node->transport->init_transport());
 
     //initialise communications
