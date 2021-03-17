f8c901d8ca6d96c85e15cd7d59dce7d1aeb8dd7e
Andreas Ramsøy <s1736273@sms.ed.ac.uk>
2021-03-17 00:14:20 +0000

    Fixed bug where node would try to broadcast to itself

--- kernel/popcorn/node_list.c
+++ kernel/popcorn/node_list.c
@@ -372,7 +372,7 @@ int add_node(struct message_node* node) { //function for adding a single node to
 
     printk(KERN_DEBUG "Setting node to be online\n");
     set_popcorn_node_online(node->index, true);
-    broadcast_my_node_info_to_node(node->index); //give them info about architecture
+    if (!(after_last_node == 1 && node->index == 0)) broadcast_my_node_info_to_node(node->index); //give them info about architecture (except if this is the only node)
 
     printk(KERN_DEBUG "Successfully added node at index %d\n", index);
 
