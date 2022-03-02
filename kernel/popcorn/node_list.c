06900b9d85933d4abfc15ba4808b59514529420c
Andreas Ramsøy <s1736273@sms.ed.ac.uk>
2022-03-02 13:34:22 +0000

    Forgot to allocate node_check_copy and to allow for multiple node list commands to be sent

--- kernel/popcorn/node_list.c
+++ kernel/popcorn/node_list.c
@@ -231,12 +231,18 @@ void check_and_repair_popcorn(void) {
 
             printk(KERN_INFO "Sending message\n");
             //copy message and then send to each neighbour (memory is freed afer message is sent so must copy)
+            node_check_copy = kmalloc(sizeof(node_check_neighbours), GFP_KERNEL);
             memcpy(node_check_copy, node_check, sizeof(node_check));
             printk(KERN_INFO "done copying message\n");
             pcn_kmsg_send(PCN_KMSG_TYPE_NODE_LIST_CHECK, previous_neighbour->index, &node_check, sizeof(node_check_neighbours));
             printk(KERN_INFO "Sent message 1\n");
             pcn_kmsg_send(PCN_KMSG_TYPE_NODE_LIST_CHECK, next_neighbour->index, &node_check_copy, sizeof(node_check_neighbours));
             printk(KERN_INFO "Sent message 2\n");
+
+            if (command != NULL) {
+                //allocate a new block of memory for the next round of conflicts to be sent
+                node_check = kmalloc(sizeof(node_check_neighbours), GFP_KERNEL);
+            }
         }
     }
     else {
